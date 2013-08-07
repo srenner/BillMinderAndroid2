856a13123d413bc5fa33070ced7e230a33caf311
Shawn Renner <shawn.renner@gmail.com>
2013-08-07 14:00:02 -0500

    Initial project creation

---
+++ src/com/srenner/billminderandroid2/BillDetailFragment.java
@@ -0,0 +1,63 @@
+package com.srenner.billminderandroid2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.srenner.billminderandroid2.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Bill detail screen. This fragment is either
+ * contained in a {@link BillListActivity} in two-pane mode (on tablets) or a
+ * {@link BillDetailActivity} on handsets.
+ */
+public class BillDetailFragment extends Fragment {
+	/**
+	 * The fragment argument representing the item ID that this fragment
+	 * represents.
+	 */
+	public static final String ARG_ITEM_ID = "item_id";
+
+	/**
+	 * The dummy content this fragment is presenting.
+	 */
+	private DummyContent.DummyItem mItem;
+
+	/**
+	 * Mandatory empty constructor for the fragment manager to instantiate the
+	 * fragment (e.g. upon screen orientation changes).
+	 */
+	public BillDetailFragment() {
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if (getArguments().containsKey(ARG_ITEM_ID)) {
+			// Load the dummy content specified by the fragment
+			// arguments. In a real-world scenario, use a Loader
+			// to load content from a content provider.
+			mItem = DummyContent.ITEM_MAP.get(getArguments().getString(
+					ARG_ITEM_ID));
+		}
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_bill_detail,
+				container, false);
+
+		// Show the dummy content as text in a TextView.
+		if (mItem != null) {
+			((TextView) rootView.findViewById(R.id.bill_detail))
+					.setText(mItem.content);
+		}
+
+		return rootView;
+	}
+}
